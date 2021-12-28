e200c3e1971c282c090a1d591b9aa098d8280cc8
pacman766 <3334976@gmail.com>
2021-12-28 16:50:21 +0300

    add api and currancies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+const BASE_URL =
+  'http://api.exchangeratesapi.io/v1/latest?access_key=fc19f25be9a53c653b0e271b9b9026ff';
+
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  console.log(currencyOptions);
+  // 1) get url api
+  // 2) convert to json
+  // 3) get base currency and arr with data of all other curr
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) =>
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      );
+  }, []);
+
   return (
     <>
       <h1>!!!Currency Converter!!!</h1>
-      <CurrencyRow />
-      <div>=</div>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions={currencyOptions} />
+      <div className="equals">=</div>
+      <CurrencyRow currencyOptions={currencyOptions} />
     </>
   );
 }
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props) {
+  const { currencyOptions } = props;
   return (
     <div>
       <input type="number" />
       <select name="" id="">
+        {currencyOptions.map((option) => (
+          <option value={option}>{option}</option>
+        ))}
         <option value="Hi">Hi</option>
       </select>
     </div>
