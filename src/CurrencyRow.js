43c9e796ced1d1018fd1f5484c0beb75a4bff649
unknown <3334976@gmail.com>
2021-12-28 13:50:05 +0300

    add markup

---
+++ src/CurrencyRow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function CurrencyRow() {
+  return (
+    <div>
+      <input type="number" />
+      <select name="" id="">
+        <option value="Hi">Hi</option>
+      </select>
+    </div>
+  );
+}
